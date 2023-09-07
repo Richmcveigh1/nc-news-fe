1f5f33750df0524867caf05065af8c45b00a340d
Rich Mcveigh <richmcveigh1@gmail.com>
2023-09-07 10:39:42 +0100

    updates so that it renders optimistically

--- src/Components/Article-card.jsx
+++ src/Components/Article-card.jsx
@@ -14,51 +14,59 @@ export default function ArticleCard({ article }) {
     created_at,
   } = article;
   const [voteCount, setVoteCount] = useState(votes);
-  const [userVote, setUserVote] = useState(0);
-console.log(voteCount)
+  const [isClicked, SetIsClicked] = useState(0);
   const date = created_at ? created_at.slice(0, 10) : "";
 
-  const handleUpvote = (userVote, setUserVote, voteCount, setVoteCount) => {
-    if (userVote === 0) {
-      updateArticleVotes(article_id, 1).then((updatedArticle) => {
-        setVoteCount(updatedArticle.votes);
-
-        setUserVote(1);
+  const handleUpvote = (isClicked, SetIsClicked, voteCount, setVoteCount) => {
+    if (isClicked === 0) {
+      setVoteCount((currentVotes) => {
+        return currentVotes + 1;
       });
-    } else if (userVote === 1) {
-      updateArticleVotes(article_id, -1).then((updatedArticle) => {
-        setVoteCount(updatedArticle.votes);
-        setUserVote(0);
+      updateArticleVotes(article_id, 1).then(() => {
+        SetIsClicked(1);
+      });
+    } else if (isClicked === 1) {
+      setVoteCount((currentVotes) => {
+        return currentVotes - 1;
+      });
+      updateArticleVotes(article_id, -1).then(() => {
+        SetIsClicked(0);
       });
     } else {
-      updateArticleVotes(article_id, 2).then((updatedArticle) => {
-        setVoteCount(updatedArticle.votes);
-        setUserVote(-1);
+        setVoteCount((currentVotes) => {
+            console.log(currentVotes)
+          return  currentVotes + 2
+        });
+      updateArticleVotes(article_id, +2).then(() => {
+        SetIsClicked(1);
       });
     }
   };
 
-  const handleDownvote = (userVote, setUserVote, voteCount, setVoteCount) => {
-    if (userVote === 0) {
-      updateArticleVotes(article_id, -1).then((updatedArticle) => {
-        setVoteCount(updatedArticle.votes);
-
-        setUserVote(-1);
+  const handleDownvote = (isClicked, SetIsClicked, voteCount, setVoteCount) => {
+    if (isClicked === 0) {
+      setVoteCount((currentVotes) => {
+        return currentVotes - 1
       });
-    } else if (userVote === -1) {
-      updateArticleVotes(article_id, 1).then((updatedArticle) => {
-        setVoteCount(updatedArticle.votes);
-        setUserVote(0);
+      updateArticleVotes(article_id, -1).then(() => {
+        SetIsClicked(-1);
+      });
+    } else if (isClicked === -1) {
+        setVoteCount((currentVotes) => {
+            return currentVotes + 1
+        });
+      updateArticleVotes(article_id, 1).then(() => {
+        SetIsClicked(0);
       });
     } else {
-      updateArticleVotes(article_id, -2).then((updatedArticle) => {
-        setVoteCount(updatedArticle.votes);
-        setUserVote(-1);
+        setVoteCount((currentVotes) => {
+            return currentVotes -2
+        });
+      updateArticleVotes(article_id, -2).then(() => {
+        SetIsClicked(-1);
       });
     }
   };
-
-
 
   return (
     <div className="article-card">
@@ -74,10 +82,10 @@ console.log(voteCount)
       <p className="article-votes">votes: {voteCount} </p>
       <button
         onClick={() =>
-          handleUpvote(userVote, setUserVote, voteCount, setVoteCount)
+          handleUpvote(isClicked, SetIsClicked, voteCount, setVoteCount)
         }
         className={`vote-button ${
-          userVote === 1 ? "highlighted-vote-button" : ""
+          isClicked === 1 ? "highlighted-vote-button" : ""
         }`}
         aria-label="upvote"
       >
@@ -85,10 +93,10 @@ console.log(voteCount)
       </button>
       <button
         onClick={() =>
-          handleDownvote(userVote, setUserVote, voteCount, setVoteCount)
+          handleDownvote(isClicked, SetIsClicked, voteCount, setVoteCount)
         }
         className={`vote-button ${
-          userVote === -1 ? "highlighted-vote-button" : ""
+          isClicked === -1 ? "highlighted-vote-button" : ""
         }`}
         aria-label="downvote"
       >
