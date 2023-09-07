23c6447e37300f0259b4abcef4ce03230c45d006
Rich Mcveigh <richmcveigh1@gmail.com>
2023-09-07 11:42:45 +0100

    adds userContext

---
+++ src/Components/User.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+export const UserContext = createContext()
+
+export const UserProvider = (props) => {
+    console.log(props)
+    return (
+        <UserContext.Provider value={ {
+            username: 'weegembump',
+            name: 'Gemma Bump',
+            avatar_url:
+              'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553'
+          }} >
+            {props.children}
+            </UserContext.Provider>
+    )
+}
